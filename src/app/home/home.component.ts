a49fbffe3bea64b757149ee069a4748d1f8c627c
pushkar <puskarkmr@gmail.com>
2020-08-08 14:42:38 +0530

    updated home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,31 @@ export class HomeComponent {
     constructor(private accountService: AccountService) {
         this.user = this.accountService.userValue;
     }
+    takeCare = "Throughout your pregnancy, your health care provider will check your weight and blood pressure while also checking the growth and development of your baby (by doing things like feeling your abdomen, listening for the fetal heartbeat starting during the second trimester, and measuring your belly). During the span of your pregnancy, you'll also have prenatal tests, including blood, urine, and cervical tests, and probably at least one ultrasound.";
+    speak(){
+        if ('speechSynthesis' in window) {
+
+            var synthesis = window.speechSynthesis;
+
+            // Get the first `en` language voice in the list
+            var voice = synthesis.getVoices().filter(function (voice) {
+                return voice.lang === 'en';
+            })[0];
+
+            // Create an utterance object
+            var utterance = new SpeechSynthesisUtterance(this.takeCare);
+
+            // Set utterance properties
+            utterance.voice = voice;
+            utterance.pitch = 1.0;
+            utterance.rate = .70;
+            utterance.volume = 1.0;
+
+            // Speak the utterance
+            synthesis.speak(utterance);
+
+        } else {
+            console.log('Text-to-speech not supported.');
+        }
+    }
 }
